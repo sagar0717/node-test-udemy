a7fccbd06394ee0dfa1feb48601e0a5a22f42a39
Sagar Kathuria <Sagar Kathuria@local.relegen.com>
2018-09-06 16:17:16 +1000

    Async Method Test cases

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -3,22 +3,65 @@ const expect = require("expect");
 
 it("should add two numbers", () => {
   var res = utils.add(3, 4);
-  expect(res).toBe(7);
-  //   if (res !== 7) {
-  //     throw new Error(`Expected 7, but got ${res}`);
-  //   }
+  expect(res)
+    .toBe(7)
+    .toBeA("number");
+});
+
+it("should async add two numbers", done => {
+  utils.asyncAdd(1, 3, sum => {
+    expect(sum)
+      .toBe(4)
+      .toBeA("number");
+    done();
+  });
 });
 
 it("should square up a number", () => {
   var res = utils.square(3);
-  if (res !== 9) {
-    throw new Error(`Expected 9, but actual is ${res}`);
-  }
+  expect(res).toBe(9);
+});
+
+it("should async square a number", done => {
+  utils.asyncSquare(3, sqr => {
+    expect(sqr)
+      .toBe(9)
+      .toBeA("number");
+    done();
+  });
 });
 
 it("should divide a number", () => {
   var res = utils.divide(6, 2);
-  if (res !== 3) {
-    throw new Error(`Expected 3, but actual is ${res}`);
-  }
+  expect(res).toBe(3);
 });
+
+it("should verify first and last name are set", () => {
+  var user = {
+    age: 25,
+    location: "India"
+  };
+  var fullName = "Sagar Kathuria";
+
+  var res = utils.setName(user, fullName);
+
+  expect(res)
+    .toInclude({ firstName: "Sagar", lastName: "Kathuria" })
+    .toBeA("object");
+});
+
+// Just playing around
+//checking some assertions
+// it("should expect some values", () => {
+//   // expect(12).toNotBe(13);
+
+//   //expect({ name: "andrew" }).toBe({ name: "andrew" }); // this will give an error as the objects are not compared like this
+
+//   //expect({ name: "Andrew" }).toEqual({ name: "Andrew" });
+
+//   //expect([2, 3, 4]).toInclude(4); // to check something exist in array
+
+//   //expect([2, 3, 4]).toExclude(4); // to check something exist in array
+
+//   //expect({ name: "Andrew", age: 25, location: "india" }).toInclude({ age: 25 });
+// });
